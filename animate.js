f3ad735c21ce8a053afc55c8cafccfc340d3f8c1
Abishek071 <abishekbca2078@prime.edu.np>
2025-02-22 18:57:17 +0545

    Animation section fixed

--- animate.js
+++ animate.js
@@ -1,54 +1,46 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
 const observer_sec = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if(entry.isIntersecting) {
-            entry.target.classList.add('appear');
-        } else {
-            entry.target.classList.remove('appear');
-        }
-    });
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("appear");
+    }
+  });
 });
 
 const elements = document.querySelectorAll(".para");
 elements.forEach((el) => observer_sec.observe(el));
 
 const observer_third = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if(entry.isIntersecting) {
-            entry.target.classList.add('conso');
-        } else {
-            entry.target.classList.remove('conso');
-        }
-    });
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("conso");
+    }
+  });
 });
 
 const element = document.querySelectorAll(".cons");
 element.forEach((el) => observer_third.observe(el));
 
 const observer_fourth = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if(entry.isIntersecting) {
-            entry.target.classList.add('glow');
-        }
-    });
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("glow");
+    }
+  });
 });
 
 const elem = document.querySelectorAll(".hide");
 elem.forEach((el) => observer_fourth.observe(el));
-
